7dff070854ef6c1df51b3d41301201631822f4cf
Saad Karkal <karkalsaad@gmail.com>
2023-05-14 21:53:41 +0100

    implements a simple shell

---
+++ simple_shell.c
@@ -0,0 +1,50 @@
+#include "minishell.h"
+/**
+ * main - implements a simple shell
+ * Souhaila Bouzradi and Saad Karkal
+ * ALX School
+ * Return: 0.
+ */
+int main(void)
+{
+	ssize_t bytes_rd = 0;
+	size_t bf_size = 0;
+	char *entry = NULL, *arguments[20];
+	int counter = 1, vf_stat = 0, exist_stat = 0, exit_stat = 0, blt_stat = 0;
+
+	_printp("$ ", 2);
+	bytes_rd = getline(&entry, &bf_size, stdin);
+	while (bytes_rd != -1)
+	{
+		if (*entry != '\n')
+		{
+			fill_args(entry, arguments);
+			if (arguments[0] != NULL)
+			{
+				exist_stat = exist(arguments[0]);
+				if (exist_stat != 0)
+				{
+					vf_stat = verify_path(arguments);
+					if (vf_stat == 0)
+						exit_stat = exec(arguments), free(entry), free(*arguments);
+					else
+					{
+						blt_stat = verify_blt(arguments, exit_stat);
+						if (blt_stat != 0)
+							exit_stat = print_not_found(arguments, counter), free(entry);
+					}
+				}
+				else
+					exit_stat = exec(arguments), free(entry);
+			}
+			else
+				free(entry);
+		}
+		else if (*entry == '\n')
+			free(entry);
+		entry = NULL, counter++;
+		_printp("$ ", 2), bytes_rd = getline(&entry, &bf_size, stdin);
+	}
+	last_free(entry);
+	return (exit_stat);
+}

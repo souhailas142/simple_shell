c848dc15e183a3275ade4aa1c990d84a6c2302f2
souhailas142 <souhailas142@gmail.com>
2023-05-15 13:06:40 +0100

    filll args

--- fill_args.c
+++ fill_args.c
@@ -10,6 +10,7 @@ int fill_args(char *entry, char **arguments)
 {
 	int i = 0;
 	char *options, *aux = entry, *command;
+
 	command = strtok(entry, "\n\t\r ");
 	arguments[i] = command;
 	while (aux != NULL)
